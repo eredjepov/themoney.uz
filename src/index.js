aab33fc8d06474045e76fbaf0ee72bd466c726df
OlyaJoss <65188310+OlyaJoss@users.noreply.github.com>
2022-05-23 13:11:52 +0500

    untracked changes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from './App';
-import {ChakraProvider} from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-const SERVER_URL = 'https://api.dollaruz.biz'
+const SERVER_URL = 'http://0.0.0.0:8080'
 
 ReactDOM.render(
   <ChakraProvider>
     <BrowserRouter>
       <React.StrictMode>
-        <Header/>
+        <Header />
         <Routes>
           <Route path='/' element={
             <App
@@ -29,10 +29,10 @@ ReactDOM.render(
                 sell: `${SERVER_URL}/rates/sell`
               }
               }
-            />}/>
+            />} />
 
         </Routes>
-        <Footer/>
+        <Footer />
       </React.StrictMode>
     </BrowserRouter>
   </ChakraProvider>,
