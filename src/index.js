c1b3c058e4779c65d61b19e2aa233d106af7d444
Aleksandr <urazaev.alexander@gmail.com>
2022-09-30 14:22:21 +0500

    apiv2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import theme from './theme'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-const SERVER_URL = 'https://api.dollaruz.biz'
+const SERVER_URL = 'https://api2.dollaruz.biz'
 
 ReactDOM.render(
   <>
